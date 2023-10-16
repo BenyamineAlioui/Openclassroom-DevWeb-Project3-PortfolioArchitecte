fae5be6fa659e8d29c30fd804e3734fcfb190ae0
Alioui <benyamine.alioui@outlook.com>
2023-10-16 13:49:59 +0200

    fin integration des deux modales

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -9,32 +9,32 @@ fetch("http://localhost:5678/api/works")
   })
   .then(data => {
     for (let i = 0; i < data.length; i++) {
-        let elements = data[i]
-    
-        const gallery = document.querySelector (".gallery")
-       
+      let elements = data[i]
 
-        /* Creation des elements */
-        const project = document.createElement ("figure")
-        const img = document.createElement ("img")
-        img.src = elements.imageUrl
-        const imgTitle = document.createElement ("figcaption")
-        imgTitle.innerText = elements.title
+      const gallery = document.querySelector(".gallery")
 
-        gallery.appendChild(project)
-      
-        project.appendChild(img)
-        project.appendChild(imgTitle)
 
-        project.classList.add("project");
-        project.setAttribute("data-category", elements.category.name);
+      /* Creation des elements */
+      const project = document.createElement("figure")
+      const img = document.createElement("img")
+      img.src = elements.imageUrl
+      const imgTitle = document.createElement("figcaption")
+      imgTitle.innerText = elements.title
 
-    } 
+      gallery.appendChild(project)
+
+      project.appendChild(img)
+      project.appendChild(imgTitle)
+
+      project.classList.add("project");
+      project.setAttribute("data-category", elements.category.name);
+
+    }
   })
-  
-  /* Filtrage des projets */
 
-  fetch("http://localhost:5678/api/categories")
+/* Filtrage des projets */
+
+fetch("http://localhost:5678/api/categories")
   .then(response => {
     if (!response.ok) {
       throw new Error('Erreur de requête réseau');
@@ -53,7 +53,7 @@ fetch("http://localhost:5678/api/works")
 
         // Filtrage des projets en fonction du nom de la catégorie
         const projects = document.querySelectorAll('.project');
-        
+
         projects.forEach(project => {
           const projectCategory = project.getAttribute('data-category');
 
@@ -70,32 +70,32 @@ fetch("http://localhost:5678/api/works")
     console.error('Une erreur s\'est produite lors de la récupération des catégories : ', error);
   });
 
-  //Affichage de la modale au clic des bouton modif
+//Affichage de la modale au clic des bouton modif
 
-  const buttonModif1 = document.querySelector('.modif_button');
-  const buttonModif2 = document.querySelector('.modif_button2');
+const buttonModif1 = document.querySelector('.modif_button');
+const buttonModif2 = document.querySelector('.modif_button2');
 
-  buttonModif1.addEventListener('click', () => {
-      const modale = document.querySelector('.bground');
-      modale.style.display = 'block';
-  })
+buttonModif1.addEventListener('click', () => {
+  const modale = document.querySelector('.bground');
+  modale.style.display = 'block';
+})
 
-  buttonModif2.addEventListener('click', () => {
-    const modale = document.querySelector('.bground');
-    modale.style.display = 'block';
-  })
+buttonModif2.addEventListener('click', () => {
+  const modale = document.querySelector('.bground');
+  modale.style.display = 'block';
+})
 
-  //Fermeture de la modale au clic de la croix
+//Fermeture de la modale au clic de la croix
 
-  const xmark = document.querySelector('.fa-xmark')
+const xmark = document.querySelector('.fa-xmark')
 
-  xmark.addEventListener('click', () => {
-    location.reload(true);
-  })
+xmark.addEventListener('click', () => {
+  location.reload(true);
+})
 
-  //Affichage des projets dans la modale
+//Affichage des projets dans la modale
 
-  fetch("http://localhost:5678/api/works")
+fetch("http://localhost:5678/api/works")
   .then(response => {
     if (!response.ok) {
       throw new Error('Erreur de requête réseau');
@@ -104,58 +104,78 @@ fetch("http://localhost:5678/api/works")
   })
   .then(data => {
     for (let i = 0; i < data.length; i++) {
-        let elements = data[i]
-    
-        const galleryModale = document.querySelector (".project-modale")
+      let elements = data[i]
 
-        /* Creation des elements */
-        const projectModale = document.createElement ("figure")
-        const img = document.createElement ("img")
-        img.src = elements.imageUrl
-        const imgTitle = document.createElement ("figcaption")
-        imgTitle.innerText = elements.title
+      const galleryModale = document.querySelector(".project-modale")
 
-        galleryModale.appendChild(projectModale)
-        projectModale.appendChild(img)
-        projectModale.appendChild(imgTitle)
+      // Creation des elements 
+      const projectModale = document.createElement("figure")
+      const img = document.createElement("img")
+      img.src = elements.imageUrl
+      const imgTitle = document.createElement("figcaption")
+      imgTitle.innerText = elements.title
 
-        projectModale.classList.add("project");
-        projectModale.setAttribute("data-category", elements.category.name);
+      galleryModale.appendChild(projectModale)
+      projectModale.appendChild(img)
+      projectModale.appendChild(imgTitle)
 
-        // Suppression des projets 
+      projectModale.classList.add("project");
+      projectModale.setAttribute("data-category", elements.category.name);
 
-        const corbeille = document.querySelector(".fa-trash-can")
-        projectModale.appendChild(corbeille)
-    } 
+      // Suppression des projets
+      const corbeille = document.createElement("i");
+      const contentCorbeille = document.createElement("div");
+      corbeille.classList.add("fa-solid", "fa-trash-can");
+      contentCorbeille.appendChild(corbeille);
+      projectModale.appendChild(contentCorbeille);
+
+      contentCorbeille.addEventListener("click", () => {
+        fetch(`http://localhost:5678/api/works/1`, {
+          method: "DELETE",
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Erreur de requête réseau");
+            }
+            return response.json(); 
+          })
+          .then((data) => {
+            projectModale.remove();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    }
   })
 
 // Gestion du mode "connecté" 
 
-  function isConnected() {
-    if (sessionStorage.getItem("token")) return true;
-    else return false;
-  }
-  
- function EditionMode() {
-    
-    const modifbarre = document.querySelector(".barre_modification");
-    const login_Logout = document.querySelector(".login_logout");
-    const modifBtn2 = document.querySelector(".modif_button2");
-    const filters = document.querySelector(".filtres");
-    
-    if (isConnected() === true) {
-      login_Logout.innerText = "logout";
-      login_Logout.addEventListener("click", () => {
-        sessionStorage.removeItem("token");
-        window.location.replace("index.html");
-      });
-      modifbarre.style.display = 'block';
-      modifBtn2.style.display = 'block';
-      filters.style.display = 'none';
-    } 
-  }
+function isConnected() {
+  if (sessionStorage.getItem("token")) return true;
+  else return false;
+}
 
-  EditionMode()
+function EditionMode() {
+
+  const modifbarre = document.querySelector(".barre_modification");
+  const login_Logout = document.querySelector(".login_logout");
+  const modifBtn2 = document.querySelector(".modif_button2");
+  const filters = document.querySelector(".filtres");
+
+  if (isConnected() === true) {
+    login_Logout.innerText = "logout";
+    login_Logout.addEventListener("click", () => {
+      sessionStorage.removeItem("token");
+      window.location.replace("index.html");
+    });
+    modifbarre.style.display = 'block';
+    modifBtn2.style.display = 'block';
+    filters.style.display = 'none';
+  }
+}
+
+EditionMode()
 
 // affichage de la modale2
 
@@ -164,10 +184,28 @@ const boutonAjouter = document.querySelector(".button1")
 boutonAjouter.addEventListener('click', () => {
   const modale = document.querySelector('.bground');
   const modale2 = document.querySelector('.bground2');
-      modale.style.display = 'none';
-      modale2.style.display = 'block';
+  modale.style.display = 'none';
+  modale2.style.display = 'block';
 })
-  
 
-  
+// Retour en arriere modale2
+const leftArrow = document.querySelector(".fa-arrow-left")
+
+leftArrow.addEventListener('click', () => {
+  const modale = document.querySelector('.bground');
+  const modale2 = document.querySelector('.bground2');
+  modale2.style.display = 'none';
+  modale.style.display = 'block';
+})
+
+// Fermeture de la seconde modale avec la seconde croix
+
+const xmark2 = document.querySelector('.xmark2')
+
+xmark2.addEventListener('click', () => {
+  location.reload(true);
+})
+
+
+
 
